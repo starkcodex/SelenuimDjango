c158abc723ee3393082f89b0665e49920ec69f56
starkcodex <maneeshemn@gmail.com>
2024-05-02 20:02:04 +0530

    Initialization of  first commit to start Selenuim django.

---
+++ SelenuimDjangoApp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SelenuimDjangoApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SelenuimDjangoApp.settings")
+
+application = get_asgi_application()
